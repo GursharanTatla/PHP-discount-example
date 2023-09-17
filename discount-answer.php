4677d5b40ab616b68e298f7b9793cecef6503f87
Gursharan Singh Tatla <gursharan.tatla@gmail.com>
2023-09-17 18:58:39 -0400

    Create discount-answer.php

---
+++ discount-answer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Product Discount Calculator</title>
+        <link rel="stylesheet" href="_css/styles.css">
+    </head>
+    <body>
+        <?php
+            // your php code goes here
+        ?>
+            
+        <div id="content">
+            <h1>Product Discount Calculator</h1>
+            <label>Product Name:</label>
+            <span><?php // display the product name here ?></span>
+            <br>
+
+            <label>Product Price:</label>
+            <span><?php // display the product price here ?></span>
+            <br>
+
+            <label>Discount Percent:</label>
+            <span><?php // display the discount percent here ?></span>
+            <br>
+
+            <label>Discounted Amount:</label>
+            <span><?php // display the discounted amount here ?></span>
+            <br>
+
+            <label>Price after Discount:</label>
+            <span><?php // display the price after discount here ?></span>
+            <br>
+        </div>
+    </body>
+</html>
